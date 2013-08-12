c12161faaa8cf7ec477a8a9e313144d7d3e154d1
PavelVais <vaispavel@gmail.com>
2013-08-12 12:14:50 +0200

    Aktualizace configu k headeru

--- application/config/header.php
+++ application/config/header.php
@@ -17,9 +17,9 @@
   |--------------------------------------------------------------------------
  */
 $config['header']['author'] = 'Pavel Vais';
-$config['header']['keywords'] = 'confession, anonymous, sharing, confess, hashtag';
+$config['header']['keywords'] = '';
 $config['header']['favicon'] = 'favicon.ico';
-$config['header']['description'] = 'Make an anonymous confession! The most interesting confessions will be made public on.';
+$config['header']['description'] = '';
 $config['header']['encode'] = 'utf-8'; //= Default
 $config['header']['language'] = 'en';  //= Default
 $config['header']['doctype'] = 'html5';  //= viz codeigniter guide ('xhtml1-strict' napr.)
@@ -30,8 +30,13 @@ $config['header']['doctype'] = 'html5';  //= viz codeigniter guide ('xhtml1-stri
  * Pokud neni uveden titulek, jako title se da POUZE title-postfix 
  */
 $config['header']['title-union'] = ' - ';
-$config['header']['title-postfix'] = 'MakeConfession.com';  //= Text, který se přidá automaticky za titulek
+$config['header']['title-postfix'] = 'My CI site';  //= Text, který se přidá automaticky za titulek
 
+/**
+ * Predpona pro cache CSS a JS
+ */
+$config['header']['cache-css-prefix'] = 'cacheminifed_'; 
+$config['header']['cache-js-prefix'] = 'cache/cacheminifed_'; 
 
 
 /*
@@ -70,7 +75,7 @@ $config['header']['title-postfix'] = 'MakeConfession.com';  //= Text, který se 
   | pokud chceme dany objekt aplikovat na jakekoli strance
   |--------------------------------------------------------------------------
   | TYTO PRAVIDLA SE VZTAHUJI KE VSEM MOZNYM PRVKUM
-  | ('meta','links','css','js')
+  | ('meta','view','css','js','string')
   |--------------------------------------------------------------------------
  */
 $config['header']['meta'][] = array(
@@ -133,26 +138,18 @@ $config['header']['css'][] = array(
     'url' => 'administration.css',
     'cover' => 'administrace',
 );
-//$config['header']['css'][] = array("jquery.smartwizard.css","jquery.ui.timepicker.css");
 
-
-/* $config['header']['caching'][] = array(
-  'name' => 'mnt_common',
-  'compress' => true,
-  'version' => "1.0.0",
-  'css' => array(
-  'css/jquery.confirm.css',
-  'css/jquery-ui',
-  "css/jquery.smartwizard.css",
-  "css/jquery-ui-1.10.0.custom.min.css",
-  "css/style.css"
-  )
-  ); */
-/*
-  $config['header']['css'][] = array(
-  'url' => 'canvas',
-  'cover' => 'canvas'
-  ); */
+/**
+  | Compressed CSS EXAMPLE
+  |--------------------------
+  | $config['header']['css'][] = array(
+  |	 'url' => array('bootstrap.css',"mystyle.css"),
+  |	 'cover' => 'all',
+  |	 'compress' => true,
+  |	 'name' => 'mystyle',
+  |	 'version' => '1.0'
+  | );
+ */
 
 
 /*
@@ -164,109 +161,29 @@ $config['header']['css'][] = array(
 //$config['header']['js'][] = 'http://cdnjs.cloudflare.com/ajax/libs/gsap/1.9.7/TweenMax.min.js';
 $config['header']['js'][] = 'http://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js';
 $config['header']['js'][] = 'plugins/jquery.confirm.js';
-
-//$config['header']['js'][] = 'plugins/jquery.superscrollorama.js';
-//$config['header']['js'][] = 'plugins/jquery.masonry.min.js';
 $config['header']['js'][] = 'ci.js';
-$config['header']['js'][] = 'plugins/jquery.charCounter.js';
-
-//$config['header']['js'][] = 'plugins/time/jquery.ui.core.min.js';
-
-
-//
-
-/*
-  $config['header']['caching'][] = array(
-  'name' => 'mnt_common',
-  'compress' => true,
-  'version' => "1.0.0",
-  'js' => array(
-  'js/plugins/jquery.actual.min.js',
-  'js/plugins/jquery.smartWizard-2.0.min.js',
-  'js/plugins/jquery.superscrollorama.js',
-  'js/plugins/jquery.autocomplete.js',
-  'js/plugins/jquery.masonry.min.js',
-  'js/ci.js'
-  ),
-
-  ); */
-
-/*
-  $config['header']['js'][] = 'plugins/time/jquery.ui.position.min.js';
-  $config['header']['js'][] = 'plugins/time/jquery.ui.widget.min.js';
-  $config['header']['js'][] = 'plugins/time/jquery.ui.tabs.min.js';
-  $config['header']['js'][] = 'plugins/jquery.ui.timepicker2.js'; */
 
 /*
   |--------------------------------------------------------------------------
-  | Hooky:
-  | Diky hookum, muzete volat cele kusy kodu, kdekoli na strance.
-  | pole 'name' urcuje nazev hooku (a nazev souboru v view/comp/header/hooks)
-  | pole 'func_name' neni povinne, urcuje nazev funkce (viz nize)
-  | pole 'cover' , 'except popr. 'restriction' jsou stejne jako jinde
-  | pole 'included' znaci, jestli bude hook automaticky vypsan
-  |                 v hlavicce. (Defaultně je TRUE)
-  | pole 'arguments' muze vlozit argument (popr. array argumenty)
-  |                  ktere pak vlozi do view souboru
-  |                  ($arg1 az argX kde X je pocet argumentu + nazev hooku)
-  | prikaz $this->header->callHeaderHook($nazevHooku); vypise dany hook
-  |        kdekoli na strance
+  | VIEWS:
+  | Diky views, muzete volat cele kusy kodu, ktere jsou ulozeny ve VIEW slozce.
+  | Hlavni argumentem musi byt cesta k VIEW souboru.
   |--------------------------------------------------------------------------
  */
-//= Animovane menu
-$config['header']['hooks'][] = array(
-    'name' => 'html5',
-);
+//= HTML5 for fuckin IE
+$config['header']['view'][] = 'comp/header/html5';
 
-$config['header']['hooks'][] = array(
-    'name' => 'google_analytics',
-);
+//= GA
+$config['header']['view'][] = 'comp/header/google_analytics';
 
 /*
   |--------------------------------------------------------------------------
-  | Cachovani:
-  |
-  |
+  | STRINGS:
+  | Diky stringum muzete do hlavicky postovat presne zneni kodu, ktere sem
+  | napisete. 
+  | 
   |--------------------------------------------------------------------------
  */
-/*
-  $config['header']['caching'][] = array(
-  'name' => 'mnt_common',
-  'compress' => true,
-  'version' => "1.0.0",
-  'js' => array(
-  'js/plugins/jquery.actual.min.js',
-  'js/plugins/jquery.smartWizard-2.0.min.js',
-  'js/plugins/jquery.superscrollorama.js',
-  'js/plugins/jquery.autocomplete.js',
-  'js/plugins/jquery.masonry.min.js',
-  'js/ci.js'
-  ),
 
-  ) */
-
-
-
-/*
-  |--------------------------------------------------------------------------
-  | Restrikce:
-  | Pokud vice objektu pouziva stejny 'cover' a 'except', muze se vytvorit
-  | restrikce, na kterou pote budou objekty odkazovat pres
-  | pole 'restriction'
-  |
-  | priklad:
-  | $config['header']['restriction'][] = array(
-  |	'name' => 'google',
-  |	'cover' => 'rozcestnik/test',
-  |	'except' => 'rozcestnik/test/bb'
-  | );
-  | pote napriklad objekt hook:
-  | $config['header']['hooks'][] = array(
-  |	'name' => 'google_analytics',
-  |	'func_name' => 'ga',
-  |	'restriction' => 'google'
-  | );
-  | Od te doby ma tento objekt stejny cover a except jako ma tato restrikce
-  |--------------------------------------------------------------------------
- */
+// $config['header']['string'][] = 'string example';
 ?>
